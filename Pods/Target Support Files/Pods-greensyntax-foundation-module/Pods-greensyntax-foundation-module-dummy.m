a69ab93675538fb014242360d5ab302b355dfdfe
Abhishek Kumar Ravi <abhishek.ravi@oyorooms.com>
2021-04-26 02:36:33 +0530

    Removed Pods

--- Pods/Target Support Files/Pods-greensyntax-foundation-module/Pods-greensyntax-foundation-module-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_greensyntax_foundation_module : NSObject
-@end
-@implementation PodsDummy_Pods_greensyntax_foundation_module
-@end
--- Pods/Target Support Files/Pods-greensyntax-foundation-moduleTests/Pods-greensyntax-foundation-moduleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_greensyntax_foundation_moduleTests : NSObject
-@end
-@implementation PodsDummy_Pods_greensyntax_foundation_moduleTests
-@end
